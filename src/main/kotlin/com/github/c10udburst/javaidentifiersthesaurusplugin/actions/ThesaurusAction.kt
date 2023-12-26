0186bd8d47d6b3fe67587d57957218140e3f33a0
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2023-12-26 11:35:27 +0100

    optimize imports

--- src/main/kotlin/com/github/c10udburst/javaidentifiersthesaurusplugin/actions/ThesaurusAction.kt
+++ src/main/kotlin/com/github/c10udburst/javaidentifiersthesaurusplugin/actions/ThesaurusAction.kt
@@ -3,26 +3,18 @@ package com.github.c10udburst.javaidentifiersthesaurusplugin.actions
 import com.github.c10udburst.javaidentifiersthesaurusplugin.Keywords
 import com.github.c10udburst.javaidentifiersthesaurusplugin.ThesaurusBundle
 import com.github.c10udburst.javaidentifiersthesaurusplugin.config.ThesaurusConfig
-import com.intellij.codeInsight.intention.IntentionAction
-import com.intellij.codeInsight.intention.IntentionActionWithChoice
-import com.intellij.codeInsight.intention.IntentionActionWithOptions
 import com.intellij.codeInsight.intention.PsiElementBaseIntentionAction
-import com.intellij.codeInsight.intention.choice.DefaultIntentionActionWithChoice
 import com.intellij.codeInsight.intention.preview.IntentionPreviewUtils
 import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.popup.PopupChooserBuilder
-import com.intellij.openapi.ui.popup.PopupStep
-import com.intellij.openapi.ui.popup.util.BaseListPopupStep
 import com.intellij.psi.JavaPsiFacade
 import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiJavaToken
 import com.intellij.psi.util.elementType
 import com.intellij.ui.components.JBList
-import com.intellij.ui.popup.list.ListPopupImpl
 
 
 class ThesaurusAction: PsiElementBaseIntentionAction() {
@@ -61,7 +53,7 @@ class ThesaurusAction: PsiElementBaseIntentionAction() {
             }
         }
 
-        builder.createPopup().showInBestPositionFor(editor!!);
+        builder.createPopup().showInBestPositionFor(editor!!)
     }
 
     private fun getOptions(keyword: String): List<String> {
